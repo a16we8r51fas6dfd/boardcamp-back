64a1d238a2eeba39e56c51e9ea709220aa8a13ae
Liaess <yann.melo@gmail.com>
2022-03-10 11:12:32 -0300

    Preparando revisão de código

--- src/routes/categoriesRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getCategories, postCategories } from "../controllers/categoriesController.js";
-import { validateCategorySchema } from "../middlewares/validateCategorySchema.js";
-
-const categoriesRouter = Router()
-categoriesRouter.get('/categories', getCategories)
-categoriesRouter.post('/categories', validateCategorySchema, postCategories)
-export default categoriesRouter
