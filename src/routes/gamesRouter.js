15384d0970508231b97fd3e23bf74174aac0fa15
YoYolops <yohanhxh@gmail.com>
2022-03-14 14:34:57 -0300

    preparando review

--- src/routes/gamesRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getGames, postGame } from "../controllers/gamesController.js";
-import validateGameSchema from "../middlewares/validateGameSchema.js";
-
-const gamesRouter = Router()
-gamesRouter.get('/games', getGames)
-gamesRouter.post('/games', validateGameSchema, postGame)
-export default gamesRouter
